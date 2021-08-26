5faea519ae079736cb0c401e2be5940d871f2199
Rafael Quinteros <rafael.quinterosvq@gmail.com>
2021-08-25 23:36:23 -0400

    trabajo para entregar

---
+++ mayor_de_3.rb
@@ -0,0 +1,14 @@
+x = ARGV[0]
+y = ARGV[1]
+z = ARGV[2]
+
+puts "Los numeros seleccionados son #{x} #{y} #{z}"
+
+if x > y and x > z 
+   puts "El numero mayor es #{x}"
+elsif y > x and y > z
+    puts "El numero mayor es #{y}"
+elsif z > x and z > y
+    puts "El numero mayor es #{z}"
+elsif puts "Hay dos numeros iguales"
+end
